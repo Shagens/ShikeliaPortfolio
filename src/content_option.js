94db806e7ab376512f29c63390d5bd9677344f7f
Shagens <Hagensshik@gmail.com>
2022-01-26 15:23:41 -0500

    added main files

---
+++ src/content_option.js
@@ -0,0 +1,90 @@
+const logotext = "SHIKELIA";
+const meta = {
+    title: "Shikelia H.",
+    description: "I’m Shikelia a full stack devloper,currently working in Berlin",
+};
+
+const introdata = {
+    title: "I’m Shikelia Hagens",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop mobile apps",
+    },
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "Bio",
+    aboutme: "Hi, My name is Shikelia Hagens, born and raised in Charlotte, NC.",
+};
+
+const skills = [{
+        name: "HTML",
+        value: 90,
+    },
+    {
+        name: "Express",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "CSS",
+        value: 85,
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "Hagensshik@gmail.com",
+    YOUR_FONE: "(980)288-2527",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    linkedin: "https://linkedin.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    skills,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
